06413c894f7a78f1fcf0cc9cc938540fd066fc23
austin-hoover <austin.michael.hoover@gmail.com>
2026-01-28 14:41:00 -0500

    Format

--- posts/eigenpainting-prl/fig_sns_paint.py
+++ posts/eigenpainting-prl/fig_sns_paint.py
@@ -14,229 +14,189 @@ from coupling import build_norm_matrix_from_tmat
 from plot import CornerGrid
 from utils import rotation_matrix
 from utils import track
+from utils import NormalizedPainter
 
 plt.style.use("style.mplstyle")
 
 
-class NormalizedPainter:
-    def __init__(
-        self,
-        nu1: float,
-        nu2: float,
-        turns: int,
-        inj_size: int,
-        inj_rms: float = 0.15,
-        inj_cut: float = 3.0,
-        method: str = "correlated",
-    ) -> None:
-        self.nu1 = nu1
-        self.nu2 = nu2
-        self.inj_size = inj_size
-        self.inj_rms = inj_rms
-        self.inj_cut = np.repeat(inj_cut, 4)
+# Parse arguments
+# --------------------------------------------------------------------------------
 
-        self.times = np.linspace(0.0, 1.0, turns + 1)
-
-        self.method = method
-        self.umax = None
-
-    def set_umax(self, umax: np.ndarray) -> None:
-        self.umax = np.array(umax)
-
-    def get_inj_point(self, turn: int) -> np.ndarray:
-        t = self.times[turn]
-        if self.method == "corr":
-            return np.multiply(self.umax, np.sqrt(t))
-        elif self.method == "anticorr":
-            tau1 = np.sqrt(1.0 - t)
-            tau2 = np.sqrt(t)
-            return np.multiply(self.umax, [tau1, tau1, tau2, tau2])
-        else:
-            raise ValueError("Invalid method")
-
-    def gen_bunch(self) -> np.ndarray:
-        return scipy.stats.truncnorm.rvs(
-            scale=self.inj_rms,
-            size=(self.inj_size, 4),
-            a=-self.inj_cut,
-            b=+self.inj_cut,
-        )
-
-    def phase_adv_matrix(self, turns: int) -> np.ndarray:
-        mu1 = 2.0 * np.pi * self.nu1 * turns
-        mu2 = 2.0 * np.pi * self.nu2 * turns
-        matrix = np.zeros((4, 4))
-        matrix[0:2, 0:2] = rotation_matrix(mu1)
-        matrix[2:4, 2:4] = rotation_matrix(mu2)
-        return matrix
-
-    def paint(self, turns: list[int]) -> np.ndarray:
-        bunches = [self.gen_bunch() for _ in range(turns + 1)]
-
-        for t in range(turns + 1):
-            bunches[t] += self.get_inj_point(t)
-
-        for t, minipulse in enumerate(tqdm(bunches)):
-            matrix = self.phase_adv_matrix(t)
-            bunches[t] = np.matmul(bunches[t], matrix.T)
-
-        return np.vstack(bunches)
+parser = argparse.ArgumentParser()
+parser.add_argument("--turns", type=int, default=2800)
+parser.add_argument("--stride", type=int, default=200)
+parser.add_argument("--inj-size", type=int, default=200)
+parser.add_argument("--inj-rms", type=float, default=0.10)
+args = parser.parse_args()
 
 
-if __name__ == "__main__":
+# Setup
+# --------------------------------------------------------------------------------
 
-    # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--turns", type=int, default=2800)
-    parser.add_argument("--stride", type=int, default=200)
-    parser.add_argument("--inj-size", type=int, default=200)
-    parser.add_argument("--inj-rms", type=float, default=0.10)
-    args = parser.parse_args()
+path = pathlib.Path(__file__)
+output_dir = os.path.join("outputs", path.stem)
+os.makedirs(output_dir, exist_ok=True)
 
-    # Create output directory
-    path = pathlib.Path(__file__)
-    output_dir = os.path.join("outputs", path.stem)
-    os.makedirs(output_dir, exist_ok=True)
+# Load transfer matrix
+M = np.loadtxt("data/matrix.txt")
+V = build_norm_matrix_from_tmat(M)
+v1, v2 = calc_eigvecs(M)
+nu1, nu2 = calc_eigtunes(M)
 
-    # Load transfer matrix
-    M = np.loadtxt("data/matrix.txt")
-    V = build_norm_matrix_from_tmat(M)
-    v1, v2 = calc_eigvecs(M)
-    nu1, nu2 = calc_eigtunes(M)
+# Create painter
+painter = NormalizedPainter(
+    nu1=nu1,
+    nu2=nu2,
+    turns=args.turns,
+    inj_size=args.inj_size,
+    inj_rms=args.inj_rms,
+)
 
-    # Create painter
-    painter = NormalizedPainter(
-        nu1=nu1,
-        nu2=nu2,
-        turns=args.turns,
-        inj_size=args.inj_size,
-        inj_rms=args.inj_rms,
-    )
+turns_list = list(range(0, args.turns + args.stride, args.stride))
 
-    # Run simulations
-    turns_list = list(range(0, args.turns + args.stride, args.stride))
+data = {}
+for method in ["corr", "anticorr", "flat", "eigen"]:
+    data[method] = {}
+    for key in ["bunch", "bunch_n", "inj", "inj_n"]:
+        data[method][key] = []
 
-    # Set max amplitude in normalized space.
-    J1 = J2 = 25.0
-    psi1 = np.pi * 0.00  # mode 1 phase
-    psi2 = np.pi * 0.00  # mode 2 phase
 
-    umax = np.zeros(4)
-    umax[0] = np.sqrt(2.0 * J1) * np.cos(psi1)
-    umax[1] = np.sqrt(2.0 * J1) * np.sin(psi1)
-    umax[2] = np.sqrt(2.0 * J2) * np.cos(psi2)
-    umax[3] = np.sqrt(2.0 * J2) * np.sin(psi2)
-    painter.set_umax(umax)
+# Run simulations
+# --------------------------------------------------------------------------------
 
-    data = {}
-    for method in ["corr", "anticorr", "flat", "eigen"]:
-        data[method] = {}
-        for key in ["bunch", "bunch_n", "inj", "inj_n"]:
-            data[method][key] = []
+# Set maximum amplitudes and phases.
+J1 = 25.0  # mode 1 amplitude
+J2 = 25.0  # mode 1 amplitude
+psi1 = np.pi * 0.00  # mode 1 phase
+psi2 = np.pi * 0.00  # mode 2 phase
 
-    painter.method = "corr"
-    data["corr"]["bunch_n"] = [painter.paint(t) for t in turns_list]
-    data["corr"]["inj_n"] = [painter.get_inj_point(t) for t in turns_list]
+# Calculate maximum phase space coordinates.
+umax = np.zeros(4)
+umax[0] = np.sqrt(2.0 * J1) * np.cos(psi1)
+umax[1] = np.sqrt(2.0 * J1) * np.sin(psi1)
+umax[2] = np.sqrt(2.0 * J2) * np.cos(psi2)
+umax[3] = np.sqrt(2.0 * J2) * np.sin(psi2)
+painter.set_umax(umax)
 
-    for method in data:
-        data[method]["bunch"] = [
-            np.matmul(bunch, V.T) for bunch in data[method]["bunch_n"]
-        ]
-        data[method]["inj"] = [np.matmul(V, u) for u in data[method]["inj_n"]]
 
-    # Make plots
-    # ----------------------------------------------------------------------
+# Run correlated painting simulation.
+painter.method = "corr"
+data["corr"]["bunch_n"] = [painter.paint(t) for t in turns_list]
+data["corr"]["inj_n"] = [painter.get_inj_point(t) for t in turns_list]
 
-    def plot_bunch(
-        bunch: np.ndarray,
-        inj_point: np.ndarray,
-        t: float,
-        limits: list[tuple[float, float]],
-        labels: list[str],
-        yscale: float = None,
-    ):
-        grid = CornerGrid(ndim=4, limits=limits, labels=labels)
-        for i in range(4):
-            for j in range(i + 1):
-                ax = grid.axs[i, j]
-                if i == j:
-                    ax.hist(
-                        bunch[:, i],
-                        bins=bins,
-                        range=limits[i],
-                        histtype="step",
-                        color="black",
-                        lw=1.3,
-                    )
-                else:
-                    values, edges = np.histogramdd(
-                        bunch[:, (j, i)], bins=bins, range=(limits[j], limits[i])
-                    )
-                    values = scipy.ndimage.gaussian_filter(values, blur)
-                    ax.pcolormesh(edges[0], edges[1], values.T, cmap="Greys", vmax=None)
+# Run anti-correlated painting simulation.
+## ...
 
-        if ymax:
-            for i in range(4):
-                grid.axs[i, i].set_ylim(0.0, ymax * 1.2)
+# Run eigenpainting simulation.
+## J2 = 0.0
+## ...
 
-        for i in range(4):
-            for j in range(i):
-                ax = grid.axs[i, j]
-                ax.scatter(
-                    inj_point[j],
-                    inj_point[i],
-                    c="red",
-                    s=8.0,
+# Unnormalize phase space coordinates
+for method in data:
+    data[method]["bunch"] = []
+    for bunch_n in data[method]["bunch_n"]:
+        bunch = np.matmul(bunch_n. V.T)
+        data[method]["bunch"].append(bunch)
+
+    data[method]["bunch"] = []
+    for u in data[method]["inj_n"]:
+        x = np.matmul(V, u)
+        data[method]["inj"].append(x)
+
+    
+# Plot data
+# ----------------------------------------------------------------------
+
+def plot_bunch(
+    bunch: np.ndarray,
+    inj_point: np.ndarray,
+    t: float,
+    limits: list[tuple[float, float]],
+    labels: list[str],
+    yscale: float = None,
+):
+    grid = CornerGrid(ndim=4, limits=limits, labels=labels)
+    for i in range(4):
+        for j in range(i + 1):
+            ax = grid.axs[i, j]
+            if i == j:
+                ax.hist(
+                    bunch[:, i],
+                    bins=bins,
+                    range=limits[i],
+                    histtype="step",
+                    color="black",
+                    lw=1.3,
                 )
+            else:
+                values, edges = np.histogramdd(
+                    bunch[:, (j, i)], bins=bins, range=(limits[j], limits[i])
+                )
+                values = scipy.ndimage.gaussian_filter(values, blur)
+                ax.pcolormesh(edges[0], edges[1], values.T, cmap="Greys", vmax=None)
 
-        grid.axs[1, 2].annotate(
-            "t = {:0.2f}".format(t),
-            xy=(0.5, 0.5),
-            xycoords="axes fraction",
-            horizontalalignment="center",
-            verticalalignment="center",
-            color="black",
-        )
-        return (grid.fig, grid.axs)
-
-    # Settings
-    bunch = data["corr"]["bunch"][-1]
-
-    xmax = 3.5 * np.std(bunch, axis=0)
-    xmax[0] = xmax[2] = max(xmax[0], xmax[2])
-    xmax[1] = xmax[3] = max(xmax[1], xmax[3])
-    limits = list(zip(-xmax, xmax))
-
-    labels = ["x", "x'", "y", "y'"]
-    bins = 64
-    cmap = "gray_r"
-    blur = 1.0
-
-    for method in data:
-        if not data[method]["bunch"]:
-            continue
-
-        output_subdir = os.path.join(output_dir, method)
-        os.makedirs(output_subdir, exist_ok=True)
-
-        last_bunch = data[method]["bunch"][-1]
-        ymax = 0.0
+    if ymax:
         for i in range(4):
-            values, edges = np.histogram(last_bunch[:, i], bins=bins, range=limits[i])
-            ymax = max(ymax, np.max(values))
+            grid.axs[i, i].set_ylim(0.0, ymax * 1.2)
 
-        for index in range(len(turns_list)):
-            bunch = data[method]["bunch"][index]
-            turn = turns_list[index]
-            inj_point = data[method]["inj"][index]
-
-            fig, axs = plot_bunch(
-                bunch=bunch,
-                inj_point=inj_point,
-                limits=limits,
-                labels=labels,
-                t=float(turn / args.turns),
+    for i in range(4):
+        for j in range(i):
+            ax = grid.axs[i, j]
+            ax.scatter(
+                inj_point[j],
+                inj_point[i],
+                c="red",
+                s=8.0,
             )
 
-            plt.savefig(os.path.join(output_subdir, f"fig_{turn:05.0f}.png"), dpi=200)
-            plt.close()
+    grid.axs[1, 2].annotate(
+        "t = {:0.2f}".format(t),
+        xy=(0.5, 0.5),
+        xycoords="axes fraction",
+        horizontalalignment="center",
+        verticalalignment="center",
+        color="black",
+    )
+    return (grid.fig, grid.axs)
+
+    
+# Settings
+bunch = data["corr"]["bunch"][-1]
+
+xmax = 3.5 * np.std(bunch, axis=0)
+xmax[0] = xmax[2] = max(xmax[0], xmax[2])
+xmax[1] = xmax[3] = max(xmax[1], xmax[3])
+limits = list(zip(-xmax, xmax))
+
+labels = ["x", "x'", "y", "y'"]
+bins = 64
+cmap = "gray_r"
+blur = 1.0
+
+for method in data:
+    if not data[method]["bunch"]:
+        continue
+
+    output_subdir = os.path.join(output_dir, method)
+    os.makedirs(output_subdir, exist_ok=True)
+
+    last_bunch = data[method]["bunch"][-1]
+    ymax = 0.0
+    for i in range(4):
+        values, edges = np.histogram(last_bunch[:, i], bins=bins, range=limits[i])
+        ymax = max(ymax, np.max(values))
+
+    for index in range(len(turns_list)):
+        bunch = data[method]["bunch"][index]
+        turn = turns_list[index]
+        inj_point = data[method]["inj"][index]
+
+        fig, axs = plot_bunch(
+            bunch=bunch,
+            inj_point=inj_point,
+            limits=limits,
+            labels=labels,
+            t=float(turn / args.turns),
+        )
+        plt.savefig(os.path.join(output_subdir, f"fig_{turn:05.0f}.png"), dpi=200)
+        plt.close()
--- posts/eigenpainting-prl/utils.py
+++ posts/eigenpainting-prl/utils.py
@@ -17,3 +17,68 @@ def track(matrix: np.ndarray, x: np.ndarray, turns: int) -> np.ndarray:
         x = np.matmul(matrix, x)
         coords[i + 1, :] = np.copy(x)
     return coords
+
+
+class NormalizedPainter:
+    def __init__(
+        self,
+        nu1: float,
+        nu2: float,
+        turns: int,
+        inj_size: int,
+        inj_rms: float = 0.15,
+        inj_cut: float = 3.0,
+        method: str = "correlated",
+    ) -> None:
+        self.nu1 = nu1
+        self.nu2 = nu2
+        self.inj_size = inj_size
+        self.inj_rms = inj_rms
+        self.inj_cut = np.repeat(inj_cut, 4)
+
+        self.times = np.linspace(0.0, 1.0, turns + 1)
+
+        self.method = method
+        self.umax = None
+
+    def set_umax(self, umax: np.ndarray) -> None:
+        self.umax = np.array(umax)
+
+    def get_inj_point(self, turn: int) -> np.ndarray:
+        t = self.times[turn]
+        if self.method == "corr":
+            return np.multiply(self.umax, np.sqrt(t))
+        elif self.method == "anticorr":
+            tau1 = np.sqrt(1.0 - t)
+            tau2 = np.sqrt(t)
+            return np.multiply(self.umax, [tau1, tau1, tau2, tau2])
+        else:
+            raise ValueError("Invalid method")
+
+    def gen_bunch(self) -> np.ndarray:
+        return scipy.stats.truncnorm.rvs(
+            scale=self.inj_rms,
+            size=(self.inj_size, 4),
+            a=-self.inj_cut,
+            b=+self.inj_cut,
+        )
+
+    def phase_adv_matrix(self, turns: int) -> np.ndarray:
+        mu1 = 2.0 * np.pi * self.nu1 * turns
+        mu2 = 2.0 * np.pi * self.nu2 * turns
+        matrix = np.zeros((4, 4))
+        matrix[0:2, 0:2] = rotation_matrix(mu1)
+        matrix[2:4, 2:4] = rotation_matrix(mu2)
+        return matrix
+
+    def paint(self, turns: list[int]) -> np.ndarray:
+        bunches = [self.gen_bunch() for _ in range(turns + 1)]
+
+        for t in range(turns + 1):
+            bunches[t] += self.get_inj_point(t)
+
+        for t, minipulse in enumerate(tqdm(bunches)):
+            matrix = self.phase_adv_matrix(t)
+            bunches[t] = np.matmul(bunches[t], matrix.T)
+
+        return np.vstack(bunches)
